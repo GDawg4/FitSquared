385ba765e4438bccc47723fa95076ddaa75d9b3c
GDawg4 <garoz.r4@gmail.com>
2018-11-25 22:52:14 -0600

    List Management 1 done Solo falta revisar rutinas Avanzando en esto falta terminar lógica para los ejercicios Falta serializar todo! Se intentó poner la elección de listas de manera bonita, probablemente se retome después

--- app/src/main/java/com/example/garoz/fitsquared/Classes/PreRoutine.kt
+++ app/src/main/java/com/example/garoz/fitsquared/Classes/PreRoutine.kt
@@ -1,9 +1,11 @@
 package com.example.garoz.fitsquared.Classes
 
+import com.example.garoz.fitsquared.Classes.Exercises.CalfRaises
+import com.example.garoz.fitsquared.Classes.Exercises.Crunches
 import com.example.garoz.fitsquared.Classes.Exercises.Exercise
 
-abstract class PreRoutine(name:String, description:String){
+abstract class PreRoutine(name:String, description:String, listOfExercise: ArrayList<Exercise>){
     val name = name
     val description = description
-    var listOfExercise: ArrayList<Exercise> = ArrayList()
+
 }
